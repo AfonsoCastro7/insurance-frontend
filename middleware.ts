3b16440a490b4d64209c5175ab42cb7cd814135d
afonso.castro <afonso.castro@rtcom.pt>
2025-12-30 15:22:47 +0000

    middleware fix

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,5 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
-  const token = request.cookies.get("token");
-
-  if (!token && request.nextUrl.pathname !== "/login") {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-
+export function middleware() {
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: ["/((?!api|_next|favicon.ico|login).*)"],
-};
